e37a69db8185611fef971ff6a1f0ad1857190254
Rasya <rasya.radja12@gmail.com>
2025-06-29 21:23:04 +0700

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,98 @@
+﻿namespace PaintDrawX
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem menuDrawOuterRing;
+        private System.Windows.Forms.ToolStripMenuItem menuDrawCenterDot;
+        private System.Windows.Forms.ToolStripMenuItem menuDrawText;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.menuDrawOuterRing = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuDrawCenterDot = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuDrawText = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.menuDrawOuterRing,
+            this.menuDrawCenterDot,
+            this.menuDrawText});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(457, 28);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // menuDrawOuterRing
+            // 
+            this.menuDrawOuterRing.Name = "menuDrawOuterRing";
+            this.menuDrawOuterRing.Size = new System.Drawing.Size(133, 24);
+            this.menuDrawOuterRing.Text = "Draw Outer Ring";
+            this.menuDrawOuterRing.Click += new System.EventHandler(this.menuDrawOuterRing_Click);
+            // 
+            // menuDrawCenterDot
+            // 
+            this.menuDrawCenterDot.Name = "menuDrawCenterDot";
+            this.menuDrawCenterDot.Size = new System.Drawing.Size(134, 24);
+            this.menuDrawCenterDot.Text = "Draw Center Dot";
+            this.menuDrawCenterDot.Click += new System.EventHandler(this.menuDrawCenterDot_Click);
+            // 
+            // menuDrawText
+            // 
+            this.menuDrawText.Name = "menuDrawText";
+            this.menuDrawText.Size = new System.Drawing.Size(89, 24);
+            this.menuDrawText.Text = "Draw Text";
+            this.menuDrawText.Click += new System.EventHandler(this.menuDrawText_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(457, 325);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Name = "Form1";
+            this.Text = "PaintDrawX";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.Paint += new System.Windows.Forms.PaintEventHandler(this.Form1_Paint);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace PaintDrawX
+{
+    public partial class Form1 : Form
+    {
+        private int drawStep = 0; // 0: nothing, 1: outer, 2: inner, 3: text
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        // Comment this out to see the annoying affect when you resize the form.
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            this.SetStyle(ControlStyles.AllPaintingInWmPaint | ControlStyles.ResizeRedraw, true);
+        }
+
+        // Menu item click handlers for drawing steps
+        private void menuDrawOuterRing_Click(object sender, EventArgs e)
+        {
+            drawStep = Math.Max(drawStep, 1);
+            this.Invalidate();
+        }
+
+        private void menuDrawCenterDot_Click(object sender, EventArgs e)
+        {
+            if (drawStep >= 1)
+            {
+                drawStep = Math.Max(drawStep, 2);
+                this.Invalidate();
+            }
+        }
+
+        private void menuDrawText_Click(object sender, EventArgs e)
+        {
+            if (drawStep >= 2)
+            {
+                drawStep = 3;
+                this.Invalidate();
+            }
+        }
+
+        // Draw the Gojek logo centered in the form.
+        private void Form1_Paint(object sender, PaintEventArgs e)
+        {
+            e.Graphics.Clear(this.BackColor);
+
+            int width = this.ClientSize.Width;
+            int height = this.ClientSize.Height - menuStrip1.Height;
+            int size = Math.Min(width, height - 60) * 3 / 4; // leave space for text
+            int centerX = width / 2;
+            int centerY = menuStrip1.Height + (height - 60) / 2 + 10;
+
+            // Outer green arc (ring with gap at the bottom)
+            if (drawStep >= 1)
+            {
+                int outerRadius = size / 2;
+                int ringThickness = (int)(outerRadius * 0.55); // much thicker, close to reference
+                Rectangle outerCircle = new Rectangle(centerX - outerRadius, centerY - outerRadius, size, size);
+                using (Pen greenPen = new Pen(Color.FromArgb(0, 184, 72), ringThickness))
+                {
+                    greenPen.StartCap = System.Drawing.Drawing2D.LineCap.Round;
+                    greenPen.EndCap = System.Drawing.Drawing2D.LineCap.Round;
+                    greenPen.Alignment = System.Drawing.Drawing2D.PenAlignment.Center;
+                    e.Graphics.SmoothingMode = System.Drawing.Drawing2D.SmoothingMode.AntiAlias;
+                    // Draw arc from 135 to 405 degrees (270 deg, leaves a gap at the bottom)
+                    e.Graphics.DrawArc(greenPen, outerCircle, 135, 270);
+                }
+            }
+
+            // Center green dot
+            if (drawStep >= 2)
+            {
+                int outerRadius = size / 2;
+                int dotRadius = (int)(outerRadius * 0.38); // much thicker, close to reference
+                int dotSize = dotRadius * 2;
+                Rectangle dotCircle = new Rectangle(centerX - dotRadius, centerY - dotRadius, dotSize, dotSize);
+                using (Brush greenBrush = new SolidBrush(Color.FromArgb(0, 184, 72)))
+                {
+                    e.Graphics.FillEllipse(greenBrush, dotCircle);
+                }
+            }
+
+            // Draw 'gojek' text
+            if (drawStep >= 3)
+            {
+                string text = "gojek";
+                using (Font font = new Font("Arial Rounded MT Bold", 40, FontStyle.Bold))
+                using (Brush blackBrush = new SolidBrush(Color.Black))
+                {
+                    SizeF textSize = e.Graphics.MeasureString(text, font);
+                    float textX = centerX - textSize.Width / 2;
+                    float textY = centerY + size / 2 - 10 + 0.08f * size;
+                    e.Graphics.DrawString(text, font, blackBrush, textX, textY);
+                }
+            }
+        }
+    }
+}
